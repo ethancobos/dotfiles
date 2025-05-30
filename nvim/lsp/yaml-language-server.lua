5548d91a425abaf58e7f1ac5bda961b83eeac298
Ethan Cobos <ecobos@amazon.com>
2025-05-30 20:27:42 +0000

    Override yaml schemas

--- nvim/lsp/yaml-language-server.lua
+++ nvim/lsp/yaml-language-server.lua
@@ -1,15 +1,22 @@
 return {
-    cmd = { 'yaml-language-server', '--stdio' },
-    filetypes = { 'yaml', 'yml', },
+    cmd = { "yaml-language-server", "--stdio" },
+    filetypes = { "yaml", "yml" },
     settings = {
         yaml = {
+            yamlVersion = 1.1,
             customTags = {
                 "!call mapping",
                 "!eval scalar",
                 "!set_override mapping",
-                "!merge sequence"
+                "!merge sequence",
+                "!map mapping",
             },
-            yamlVersion = 1.1
+            schemas = {
+                ["https://json.schemastore.org/yamllint.json"] = {
+                    "*.yaml",
+                    "*.yml",
+                },
+            },
         },
         redhat = { telemetry = { enabled = false } },
     },
