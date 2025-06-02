427b5759aa361a0be9ec93b9478416acd6bee97e
Ethan Cobos <ecobos@amazon.com>
2025-06-02 20:16:19 +0000

    Add insert mode navigation keybinds

--- nvim/lua/configs/keymaps.lua
+++ nvim/lua/configs/keymaps.lua
@@ -16,52 +16,58 @@ map("n", "<leader>ch", "<cmd>NvCheatsheet<CR>", { desc = "toggle nvcheatsheet" }
 -- │                  Navigation                  │
 -- ╰──────────────────────────────────────────────╯
 
-map("i", "<C-b>", "<ESC>^i", { desc = "move beginning of line" })
-map("i", "<C-e>", "<End>", { desc = "move end of line" })
+-- Normal mode
 map("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>", { desc = "window left" })
 map("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>", { desc = "window down" })
 map("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>", { desc = "window up" })
 map("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>", { desc = "window right" })
 
+-- Insert mode
+map("i", "<C-a>", "<ESC>^i", { desc = "move beginning of line" })
+map("i", "<C-e>", "<End>", { desc = "move end of line" })
+map("i", "<C-h>", "<Left>", { desc = "move left" })
+map("i", "<C-j>", "<Down>", { desc = "move down" })
+map("i", "<C-k>", "<Up>", { desc = "move up" })
+map("i", "<C-l>", "<Right>", { desc = "move right" })
 -- ╭──────────────────────────────────────────────╮
 -- │                  Telescope                   │
 -- ╰──────────────────────────────────────────────╯
 
-local builtin = require 'telescope.builtin'
-map('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-map('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-map('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-map('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-map('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-map('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-map('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-map('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-map('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-map('n', '<leader>sb', builtin.buffers, { desc = '[S]earch [B]uffers' })
+local builtin = require("telescope.builtin")
+map("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+map("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+map("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+map("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+map("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+map("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+map("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+map("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+map("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+map("n", "<leader>sb", builtin.buffers, { desc = "[S]earch [B]uffers" })
 map("n", "<leader>sc", builtin.git_commits, { desc = "[S]earch [C]ommits" })
 map("n", "<leader>st", builtin.git_status, { desc = "[S]earch s[T]atus" })
 
-map('n', '<leader>/', function()
+map("n", "<leader>/", function()
     -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-    builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+    builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
         winblend = 10,
         previewer = false,
-    })
-end, { desc = '[/] Fuzzily search in current buffer' })
+    }))
+end, { desc = "[/] Fuzzily search in current buffer" })
 
 -- It's also possible to pass additional configuration options.
 --  See `:help telescope.builtin.live_grep()` for information about particular keys
-vim.keymap.set('n', '<leader>s/', function()
-    builtin.live_grep {
+vim.keymap.set("n", "<leader>s/", function()
+    builtin.live_grep({
         grep_open_files = true,
-        prompt_title = 'Live Grep in Open Files',
-    }
-end, { desc = '[S]earch [/] in Open Files' })
+        prompt_title = "Live Grep in Open Files",
+    })
+end, { desc = "[S]earch [/] in Open Files" })
 
 -- Shortcut for searching your Neovim configuration files
-vim.keymap.set('n', '<leader>sn', function()
-    builtin.find_files { cwd = vim.fn.stdpath 'config' }
-end, { desc = '[S]earch [N]eovim files' })
+vim.keymap.set("n", "<leader>sn", function()
+    builtin.find_files({ cwd = vim.fn.stdpath("config") })
+end, { desc = "[S]earch [N]eovim files" })
 
 -- ╭──────────────────────────────────────────────╮
 -- │                  Conform                     │
