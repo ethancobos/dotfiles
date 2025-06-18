93c3a789754c546474746e93c82f2a417238afe5
Ethan Cobos <ethancobos2@outlook.com>
2025-06-18 01:31:47 -0700

    Add keybinds for Trouble

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -4,8 +4,13 @@ return {
     cmd = "Trouble",
     keys = {
         {
-            "<leader>tt",
+            "<leader>tT",
             "<cmd>Trouble diagnostics toggle<cr>",
+            desc = "[T]oggle Every [T]rouble",
+        },
+        {
+            "<leader>tt",
+            "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
             desc = "[T]oggle [T]rouble",
         },
     },
