7870f79876f8f6e043f5c3efe78ea1b7ebb6a1fb
Ethan Cobos <ecobos@amazon.com>
2025-07-02 17:51:49 +0000

    Add ruby 3.4

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -2,23 +2,33 @@ return {
     "stevearc/conform.nvim",
     event = { "BufWritePre" },
     cmd = { "ConformInfo" },
+    keys = {
+        {
+            "<leader>f",
+            function()
+                require("conform").format({ async = true })
+            end,
+            mode = "",
+            desc = "Format buffer",
+        },
+    },
     opts = {
         notify_on_error = false,
-        format_on_save = {
-            timeout_ms = 500,
-            lsp_format = "fallback",
-        },
+        -- format_on_save = {
+        --     timeout_ms = 500,
+        --     lsp_format = "fallback",
+        -- },
         formatters_by_ft = {
             lua = { "stylua" },
             bash = { "shfmt" },
             json = { "jq" },
             python = {
-                -- "ruff_format",
+                "ruff_format",
                 "ruff_organize_imports",
             },
             rust = { "rustfmt" },
-            -- java = { "google-java-format" },
-            -- ruby = { "rubocop" },
+            java = { "google-java-format" },
+            ruby = { "rubocop" },
         },
     },
 }
